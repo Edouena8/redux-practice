2baf52ae9ad32da68524f10c671a5b456f690019
Edouena8 <103317047+Edouena8@users.noreply.github.com>
2023-04-18 13:18:00 +0200

    log in and log out of user

---
+++ src/components/UserMenu.jsx
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "redux/userSlice";
+
+export const UserMenu = () => {
+    const login = useSelector(state => state.user.login);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            {login}
+            <button type="button" onClick={() => dispatch(logOut())}>Log out</button>
+        </div>
+    );
+};
